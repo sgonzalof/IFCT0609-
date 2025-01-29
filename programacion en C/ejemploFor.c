f1b1f156313bce022afe83cc6526eaf0de235a68
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-01-29 08:48:36 +0100

    cambio nombre ejercicio bucle for

--- programacion en C/ejemploFor.c
+++ programacion en C/ejemploFor.c
@@ -8,7 +8,7 @@ void incrementa(){
 int main(void)
 {
     int cont;
-    for(cont=1;cont <= 3; cont++) 
+    for(cont=1;cont<= 3; cont++) 
     { /* Inicializo bucle en la 1a vuelta a cont=1,*/
     printf("Iteracion %d\t", cont);/* incrementa en cada vuelta el cont a 1
     (cont++)*/
