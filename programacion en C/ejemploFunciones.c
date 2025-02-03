604929d2e48d6829187292ad5d74c14bdad30d9d
saraGlez1980 <sara.glez.cuervo@gmail.com>
2025-02-03 10:19:16 +0100

    revision soluciones punteros

--- programacion en C/ejemploFunciones.c
+++ programacion en C/ejemploFunciones.c
@@ -1,9 +1,25 @@
-int Comparacion(int a,int b) 
+/* */
 
-{ 
+#include <stdio.h>
+#include <math.h>
 
-if (a>b) return 1; /* a es mayor que b */ 
-if (a<b) return -1; /* a es menor que b */ 
-return 0; /* a y b son iguales */ 
+int pasoPorValor(int a,int b) 
+
+{  
+a++;
+b++;
+printf("El valor de a y b en el ambito de la funcion es: %d %d ",a,b);
+return a+b;
 
 } 
+
+int main(void)
+{
+    int x,y,suma;
+    x=0;
+    y=0;
+    suma=pasoPorValor(x,y);
+    printf("\nEl valor de x e y tras invocar a  la funcion es: %d %d  ",x,y);
+    printf("\nLa suma es igual a %d ",suma);
+    return 0;
+}
